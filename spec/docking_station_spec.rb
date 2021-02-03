b3d73279a0e53a49de42f5724340465dc6fab8b7
Nicolas Bermejo <nicbermejo@gmail.com>
2021-02-03 17:22:39 +0000

    boris bike step 12

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,6 +7,20 @@ describe DockingStation do
     end
   end
 
+  it 'docks the current bike to the docking station' do
+      expect(subject).to respond_to(:dock).with(1).argument
+  end
+  
+  it 'docks bike' do
+      bike = Bike.new
+      expect(subject.dock(bike)).to eq bike
+  end
+  
+  it 'returns contained bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq(bike)
+  end
 
   it 'responds to release_bike' do
     expect(subject).to respond_to :release_bike
