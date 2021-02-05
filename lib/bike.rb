8af0ff10f1fa7c97831b0e1de8a2f291ce951157
Joey Hornsby <joey.s.hornsby@gmail.com>
2021-02-05 16:26:49 +0000

    added method to report bike broken

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,13 @@
 class Bike
-  def working?
-    true
+  attr_reader :working
+
+  def initialize
+    @working = true
+  end
+
+  def report_broken
+    @working = false
+    return self
   end
 
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,15 @@
 require 'bike'
 
 describe Bike do
-  it 'responds to working?' do
-    expect(subject).to respond_to :working?
+
+  it { is_expected.to respond_to :report_broken}
+
+  it 'responds to working' do
+    expect(subject).to respond_to :working
   end
+
+  it 'working returns false if report_broken has been called' do
+    expect(subject.report_broken.working).to eq false
+  end
+
 end
